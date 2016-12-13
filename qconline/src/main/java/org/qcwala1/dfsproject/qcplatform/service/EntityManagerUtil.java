60cca92575408e44550a2487434f2fc52b7d2012
Chirag# <Chirag#@Chirag>
2016-12-13 20:15:38 +0530

    Second commit

--- qconline/src/main/java/org/qcwala1/dfsproject/qcplatform/service/EntityManagerUtil.java
+++ qconline/src/main/java/org/qcwala1/dfsproject/qcplatform/service/EntityManagerUtil.java
@@ -1,5 +1,10 @@
 package org.qcwala1.dfsproject.qcplatform.service;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -7,11 +12,34 @@ import javax.persistence.Persistence;
 public class EntityManagerUtil {
 	private static EntityManagerFactory entityManagerFactory;
 	static {
+		Map<String, String> persistenceMap = new HashMap<>();
+		URI dbUri = null;
 		try {
-			entityManagerFactory = Persistence.createEntityManagerFactory("QCportalPU");
+			dbUri = new URI(System.getenv("DATABASE_URL"));
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		if (dbUri != null) {
 
-		} catch (Throwable ex) {
-			throw new ExceptionInInitializerError(ex);
+			String username = dbUri.getUserInfo().split(":")[0];
+			String password = dbUri.getUserInfo().split(":")[1];
+			String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+			persistenceMap.put("javax.persistence.jdbc.url", dbUrl);
+			persistenceMap.put("javax.persistence.jdbc.user", username);
+			persistenceMap.put("javax.persistence.jdbc.password", password);
+			persistenceMap.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+			persistenceMap.put("javax.persistence.schema-generation.database.action", "create");
+			try {
+				// entityManagerFactory =
+				// Persistence.createEntityManagerFactory("QCportalPU");
+				entityManagerFactory = Persistence.createEntityManagerFactory("<current persistence unit>",
+						persistenceMap);
+
+			} catch (Throwable ex) {
+				throw new ExceptionInInitializerError(ex);
+			}
+
 		}
 	}
 
